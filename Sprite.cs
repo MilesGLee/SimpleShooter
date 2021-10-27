208c0f3515a2600b968793d952f3fc22e3955c56
Miles <miles.gr.lee@gmail.com>
2021-10-27 12:22:32 -0500

    sprite commit 1.0

---
+++ Sprite.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibrary;
+using Raylib_cs;
+
+namespace SimpleShooter
+{
+    class Sprite
+    {
+        private Texture2D _texture;
+
+        //Width of the loaded texture
+        public int Width 
+        {
+            get { return _texture.width; }
+            private set { _texture.width = value; }
+        }
+        //Height of the loaded texture
+        public int Height 
+        {
+            get { return _texture.height; }
+            private set { _texture.height = value; }
+        }
+
+        //sets the texture with the given folder path.
+        public Sprite(string path) 
+        {
+            _texture = Raylib.LoadTexture(path);
+        }
+
+        //Draws the sprite using the rotation, translation and scale of the given transform.
+        public void Draw(Matrix3 transform) 
+        {
+            //Finds the scale of the sprite.
+            Width = (int)Math.Round(new Vector2(transform.M00, transform.M10).Magnitude);
+            Height = (int)Math.Round(new Vector2(transform.M01, transform.M11).Magnitude);
+
+
+        }
+    }
+}
